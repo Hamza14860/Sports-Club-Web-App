a9b050f334a2cb3193d8055ab8e57372c4326724
Hamza A <42303641+Hamza14860@users.noreply.github.com>
2020-05-21 22:31:56 +0500

    Admin Module Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+import { PlayerCreateComponent } from './admin-module/player-create/player-create.component';
+import { PlayerEditComponent } from './admin-module/player-edit/player-edit.component';
+import { PlayerListComponent } from './admin-module/player-list/player-list.component';
+import { AdminHomeComponent} from './admin-module/admin-home/admin-home.component';
+
+
+const routes: Routes = [
+  { path: 'admin-home', component: AdminHomeComponent } ,
+  // { path: '', pathMatch: 'full', redirectTo: 'create-player' },
+  { path: 'create-player', component: PlayerCreateComponent },
+  { path: 'edit-player/:id', component: PlayerEditComponent },
+  { path: 'players-list', component: PlayerListComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
