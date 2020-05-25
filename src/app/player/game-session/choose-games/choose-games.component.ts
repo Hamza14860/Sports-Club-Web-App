0c46aadc0eba5bf4f00b5cd8c0c0d648bf5ce8b6
Hamza A <42303641+Hamza14860@users.noreply.github.com>
2020-05-25 12:58:04 +0500

    Game Session Components Designed

--- src/app/player/game-session/choose-games/choose-games.component.ts
+++ src/app/player/game-session/choose-games/choose-games.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-choose-games',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-games.component.css']
 })
 export class ChooseGamesComponent implements OnInit {
+  gameForm:FormGroup;
+  public time: string = null;
 
-  constructor() { }
+
+  games: string[] = [
+    'Tennis', 'Badminton', 'Table Tennis', 'Squash'
+  ];
+  gameControl = new FormControl(this.games[0]);
+  timeControl = new FormControl();
+
+  constructor() {
+    this.gameForm = new FormGroup({
+      game: this.gameControl,
+      time: this.timeControl
+
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.gameControl.value);
+    console.log(this.timeControl.value );
+
   }
 
 }
