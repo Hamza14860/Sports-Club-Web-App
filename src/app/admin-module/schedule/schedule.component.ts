8a429067ea532e7f0270aa7393d7bc135aa62587
Hamza A <42303641+Hamza14860@users.noreply.github.com>
2020-05-31 05:23:37 +0500

    Bug Removed, Admin Wallpaper changed

--- src/app/admin-module/schedule/schedule.component.ts
+++ src/app/admin-module/schedule/schedule.component.ts
@@ -8,6 +8,8 @@ import { Router } from "@angular/router";
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   constructor(public userService: UserService,private router : Router) { }
 
   showSucessMessage: Boolean;
